a5b790796e8cee92fb761779d94bb7d9875214b7
erikchiodo <erikchiodo@gmail.com>
2023-03-12 19:31:25 -0400

    Refining lat/long logic and setting up method for grabbing weather information

--- assets/script.js
+++ assets/script.js
@@ -9,7 +9,6 @@ console.log(searchCity);
 // testUrl = http://api.openweathermap.org/geo/1.0/direct?q=NewYork&appid=ba368f94dc661d16d9e70c0b63cd68a9
 
 $(function () {
-
   function getGeoCode(city) {
     var geoURL =
       "http://api.openweathermap.org/geo/1.0/direct?q=" +
@@ -17,18 +16,44 @@ $(function () {
       "&appid=ba368f94dc661d16d9e70c0b63cd68a9";
     console.log(geoURL);
     fetch(geoURL)
-        .then(function (response) {
-      if (response.ok) {
-        return response.json();
-      } else {
-        alert("Unable to retrieve data");
-        return;
-      }});
-      .then(function (data) {
-      console.log(data);
-  })
-  
-    $("#submit-btn").on("click", function (event) {
+      .then(function (response) {
+        if (response.ok) {
+          return response.json();
+        } else {
+          alert("Unable to retrieve data");
+          return;
+        }
+      })
+      .then(function (geoData) {
+        console.log(geoData);
+        var locationLat = geoData[1];
+        console.log(locationLat);
+        var locationLong = geoData[2];
+        console.log(locationLong);
+        getWeather(locationLat, locationLong);
+      });
+  }
+  function getWeather(lat, long) {
+    var weatherURL =
+      "api.openweathermap.org/data/2.5/forecast?lat=" +
+      lat +
+      "&lon=" +
+      long +
+      "&appid=ba368f94dc661d16d9e70c0b63cd68a9";
+    console.log(weatherURL);
+    fetch(weatherURL)
+      .then(function (response) {
+        if (response.ok) {
+          return response.json();
+        } else {
+          alert("Unable to retrieve data");
+        }
+      })
+      .then(function (weatherData) {
+        console.log(weatherData);
+      });
+  }
+  $("#submit-btn").on("click", function (event) {
     event.preventDefault();
     var city = searchCity.val();
     console.log(city);
@@ -38,4 +63,4 @@ $(function () {
       alert("Please Try again");
     }
   });
-}});
+});
